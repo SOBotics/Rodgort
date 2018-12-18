daba40fb78ac3d33f3a0712669fea442b93aa806
Robert Rudman <rjrudman@gmail.com>
2018-12-18 22:38:33 +1100

    Implement SlidingBuffer for future use

---
+++ Rodgort/Utilities/ReactiveX/Operators.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+
+namespace Rodgort.Utilities.ReactiveX
+{
+    public static class Operators
+    {
+        public static IObservable<IList<TSource>> SlidingBuffer<TSource>(this IObservable<TSource> source, TimeSpan timeSpan)
+        {
+            return source.Buffer(() =>
+            {
+                return source.Select(a => Observable.Timer(timeSpan)).Switch();
+            });
+        }
+    }
+}
