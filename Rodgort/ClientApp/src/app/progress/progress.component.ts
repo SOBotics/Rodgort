b2c993a101e402bdaaa063ac29b71172c815c82d
Robert Rudman <rjrudman@gmail.com>
2018-12-21 14:51:02 +1100

    Fix build

--- Rodgort/ClientApp/src/app/progress/progress.component.ts
+++ Rodgort/ClientApp/src/app/progress/progress.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Chart } from 'angular-highcharts';
-import Highcharts = require('highcharts');
 
 @Component({
   selector: 'app-progress',
