835eaae22f424f57dd82a146b25f23a34624935e
Robert Rudman <rjrudman@gmail.com>
2018-12-27 14:09:36 +1100

    Send the token for unknown deletions

--- Rodgort/ClientApp/src/app/unknown-deletion-resolution/unknown-deletion-resolution.component.ts
+++ Rodgort/ClientApp/src/app/unknown-deletion-resolution/unknown-deletion-resolution.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-unknown-deletion-resolution',
@@ -13,7 +14,7 @@ export class UnknownDeletionResolutionComponent implements OnInit {
 
   private userIdRegex = /\/users\/(\d+)/g;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private authService: AuthService) { }
 
   ngOnInit() {
     const checkerInterval = setInterval(() => {
@@ -21,103 +22,111 @@ export class UnknownDeletionResolutionComponent implements OnInit {
       if (untypedWindow.getRevision) {
         this.getRevision = untypedWindow.getRevision;
         clearInterval(checkerInterval);
-
-        this.httpClient.get('/api/admin/UnresolvedDeletions')
-          .subscribe((data: any) => {
-            this.postsToVisit = data.map(action => ({
-              actionId: action.actionId,
-              postId: action.postId,
-              info: '',
-              status: 'pending'
-            }));
-
-            let i = 0;
-            const processNext = () => {
-              if (i >= this.postsToVisit.length - 1) {
-                return;
-              }
-
-              const postToVisit = this.postsToVisit[i];
-              i++;
-              postToVisit.status = 'processing';
-              const revisionInfoPromise = this.getRevision(postToVisit.postId);
-              revisionInfoPromise.then(revisionInfo => {
-                const container = document.implementation.createHTMLDocument('').documentElement;
-                container.innerHTML = revisionInfo.responseText;
-
-                let requests = [];
-                const revisions = Array.from(container.querySelectorAll('#revisions tr'));
-                revisions.forEach(element => {
-                  if (element.getAttribute('class') === 'revision') {
-                    const revisionDateInfo = element.querySelector('.revcell4 .relativetime');
-                    const date = revisionDateInfo.getAttribute('title');
-                    const userInfo = element.querySelector('.revcell4 .user-details > a');
-
-                    const match = userInfo.getAttribute('href').match(/\/users\/(\d+)/);
-                    const userId = parseInt(match[1], 10);
-
-                    const postTags = Array.from(element.nextElementSibling.querySelectorAll('.post-tag > span'));
-                    if (postTags.length <= 0) {
-                      return;
-                    }
-
-                    requests = postTags.map(e => {
-                      return {
-                        actionId: postToVisit.actionId,
-                        userId,
-                        tag: e.innerHTML,
-                        actionTypeId: e.getAttribute('class') === 'diff-delete' ? 1 : 2,
-                        dateTime: date
-                      };
-                    });
-                  } else if (element.getAttribute('class') === 'vote-revision') {
-                    const revisionVoteInfo = element.querySelector('.revcell3');
-                    const xml = revisionVoteInfo.outerHTML;
-
-                    let actionTypeId = -1;
-                    if (xml.indexOf('<b>Post Closed</b>') > 0) {
-                      actionTypeId = 3;
-                    } else if (xml.indexOf('<b>Post Reopened</b>') > 0) {
-                      actionTypeId = 4;
-                    } else if (xml.indexOf('<b>Post Deleted</b>') > 0) {
-                      actionTypeId = 5;
-                    } else if (xml.indexOf('<b>Post Undeleted</b>') > 0) {
-                      actionTypeId = 6;
-                    }
-                    if (actionTypeId === -1) {
-                      return;
-                    }
-
-                    const revisionDateInfo = element.querySelector('.revcell4 .relativetime');
-                    const date = revisionDateInfo.getAttribute('title');
-
-                    let userIdMatch = this.userIdRegex.exec(xml);
-                    while (userIdMatch != null) {
-                      const userId = parseInt(userIdMatch[1], 10);
-                      requests.push({
-                        actionId: postToVisit.actionId,
-                        userId,
-                        actionTypeId,
-                        dateTime: date
-                      });
-                      userIdMatch = this.userIdRegex.exec(xml);
-                    }
-                  }
-                });
-
-                if (requests.length) {
-                  this.httpClient.post('/api/admin/ResolveUnresolvedDeletion', requests)
-                    .subscribe(d => {
-                      postToVisit.status = 'done';
-                      setTimeout(processNext, 5000);
-                    });
-                }
-              });
-            };
-
-            processNext();
-          });
       }
     }, 500);
+  }
+
+  public startProcessing() {
+    this.authService.GetAuthDetails().subscribe(d => {
+      this.httpClient.get('/api/admin/UnresolvedDeletions', {
+        headers: { 'Authorization': 'Bearer ' + d.RawToken }
+      })
+        .subscribe((data: any) => {
+          this.postsToVisit = data.map(action => ({
+            actionId: action.actionId,
+            postId: action.postId,
+            info: '',
+            status: 'pending'
+          }));
+
+          let i = 0;
+          const processNext = () => {
+            if (i >= this.postsToVisit.length - 1) {
+              return;
+            }
+
+            const postToVisit = this.postsToVisit[i];
+            i++;
+            postToVisit.status = 'processing';
+            const revisionInfoPromise = this.getRevision(postToVisit.postId);
+            revisionInfoPromise.then(revisionInfo => {
+              const container = document.implementation.createHTMLDocument('').documentElement;
+              container.innerHTML = revisionInfo.responseText;
+
+              let requests = [];
+              const revisions = Array.from(container.querySelectorAll('#revisions tr'));
+              revisions.forEach(element => {
+                if (element.getAttribute('class') === 'revision') {
+                  const revisionDateInfo = element.querySelector('.revcell4 .relativetime');
+                  const date = revisionDateInfo.getAttribute('title');
+                  const userInfo = element.querySelector('.revcell4 .user-details > a');
+
+                  const match = userInfo.getAttribute('href').match(/\/users\/(\d+)/);
+                  const userId = parseInt(match[1], 10);
+
+                  const postTags = Array.from(element.nextElementSibling.querySelectorAll('.post-tag > span'));
+                  if (postTags.length <= 0) {
+                    return;
+                  }
+
+                  requests = postTags.map(e => {
+                    return {
+                      actionId: postToVisit.actionId,
+                      userId,
+                      tag: e.innerHTML,
+                      actionTypeId: e.getAttribute('class') === 'diff-delete' ? 1 : 2,
+                      dateTime: date
+                    };
+                  });
+                } else if (element.getAttribute('class') === 'vote-revision') {
+                  const revisionVoteInfo = element.querySelector('.revcell3');
+                  const xml = revisionVoteInfo.outerHTML;
+
+                  let actionTypeId = -1;
+                  if (xml.indexOf('<b>Post Closed</b>') > 0) {
+                    actionTypeId = 3;
+                  } else if (xml.indexOf('<b>Post Reopened</b>') > 0) {
+                    actionTypeId = 4;
+                  } else if (xml.indexOf('<b>Post Deleted</b>') > 0) {
+                    actionTypeId = 5;
+                  } else if (xml.indexOf('<b>Post Undeleted</b>') > 0) {
+                    actionTypeId = 6;
+                  }
+                  if (actionTypeId === -1) {
+                    return;
+                  }
+
+                  const revisionDateInfo = element.querySelector('.revcell4 .relativetime');
+                  const date = revisionDateInfo.getAttribute('title');
+
+                  let userIdMatch = this.userIdRegex.exec(xml);
+                  while (userIdMatch != null) {
+                    const userId = parseInt(userIdMatch[1], 10);
+                    requests.push({
+                      actionId: postToVisit.actionId,
+                      userId,
+                      actionTypeId,
+                      dateTime: date
+                    });
+                    userIdMatch = this.userIdRegex.exec(xml);
+                  }
+                }
+              });
+
+              if (requests.length) {
+                this.httpClient.post('/api/admin/ResolveUnresolvedDeletion', requests, {
+                  headers: { 'Authorization': 'Bearer ' + d.RawToken }
+                })
+                  .subscribe(d => {
+                    postToVisit.status = 'done';
+                    setTimeout(processNext, 5000);
+                  });
+              }
+            });
+          };
+
+          processNext();
+        });
+    });
   }
 }
