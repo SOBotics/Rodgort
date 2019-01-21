fa7d577a01db8f2f95b150656afead46eb79c5f8
Robert Rudman <rjrudman@gmail.com>
2019-01-21 16:50:02 +1100

    Better filtering of logs

--- Rodgort/Controllers/LogsController.cs
+++ Rodgort/Controllers/LogsController.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Rodgort.Data;
 using Rodgort.Data.Tables;
 using Rodgort.Utilities.Paging;
@@ -23,8 +25,15 @@ namespace Rodgort.Controllers
             if (!string.IsNullOrWhiteSpace(search))
                 query = query.Where(q => q.Message.Contains(search) || q.Exception.Contains(search));
             if (!string.IsNullOrWhiteSpace(level))
-                query = query.Where(q => q.Level == level);
+            {
+                var enumList = new[] {"Trace", "Debug", "Info", "Warn", "Error", "Fatal"}.Select((e, i) => new {Name = e, Value = i}).ToList();
 
+                var matchedLevel = enumList.First(e => e.Name == level) ?? enumList.First();
+                var desiredLevel = matchedLevel.Value;
+
+                var lookFor = enumList.Where(e => e.Value > desiredLevel).Select(e => e.Name).ToList();
+                query = query.Where(q => lookFor.Contains(q.Level));
+            }
             var orderedQuery = query.OrderByDescending(l => l.TimeLogged);
             var result = orderedQuery.Page(page, pageSize);
             return result;
