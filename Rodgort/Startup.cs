8de4624fed8148bc5dfb98987b7f16fd7d035d85
Robert Rudman <rjrudman@gmail.com>
2019-01-21 22:53:11 +1100

    Revert "Attempt to fix anti forgery error"
    This reverts commit 89c214cdf123425a8c7f96ba6b1b8683392dbc40.

--- Rodgort/Startup.cs
+++ Rodgort/Startup.cs
@@ -35,8 +35,6 @@ namespace Rodgort
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDataProtection();
-
             var connectionString = Configuration.GetConnectionString("RodgortDB");
             GlobalDiagnosticsContext.Set("connectionString", connectionString);
 
