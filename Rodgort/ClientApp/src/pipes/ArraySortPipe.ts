ef29266d00d38dee0014b18ee54b9554c11a6680
Robert Rudman <rjrudman@gmail.com>
2019-01-22 11:57:06 +1100

    Allow sort pipe to accept multiple fields. Sort tags by tracking status and then by name

--- Rodgort/ClientApp/src/pipes/ArraySortPipe.ts
+++ Rodgort/ClientApp/src/pipes/ArraySortPipe.ts
@@ -4,21 +4,32 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'sort'
 })
-export class ArraySortPipe  implements PipeTransform {
-  transform(array: any, field: string): any[] {
+export class ArraySortPipe implements PipeTransform {
+  transform(array: any, fields: string): any[] {
+    const fieldsArr = fields.split(',');
     if (!Array.isArray(array)) {
       return;
     }
     array.sort((a: any, b: any) => {
-      if (a[field] < b[field]) {
-        return -1;
-      } else if (a[field] > b[field]) {
-        return 1;
-      } else {
-        return 0;
-      }
+      let fieldIndex = 0;
+      let sortResult = 0;
+      do {
+        const field = fieldsArr[fieldIndex++];
+        sortResult = this.sort(a, b, field);
+      } while (sortResult === 0 && fieldIndex < fieldsArr.length);
+      return sortResult;
     });
 
     return array;
+  }
+
+  sort(a: any, b: any, field: string) {
+    if (a[field] < b[field]) {
+      return -1;
+    } else if (a[field] > b[field]) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 }
