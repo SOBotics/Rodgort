28c3b88851644aecdcf4f42c2e84331d250a6369
Robert Rudman <rjrudman@gmail.com>
2019-02-14 12:10:28 +1100

    Automatically reconnect closed websockets

--- Rodgort/ClientApp/src/utils/WebsocketHelper.ts
+++ Rodgort/ClientApp/src/utils/WebsocketHelper.ts
@@ -5,19 +5,24 @@ export class WebsocketHelper<T> {
     public Observable: Observable<T>;
     constructor(url: string) {
         this.Observable = Observable.create((obs: Observer<T>) => {
-            const websocket = new WebSocket(url);
-            websocket.onmessage = event => {
-                if (event.data === 'ping') {
-                    websocket.send('pong');
-                } else {
-                    const data = JSON.parse(event.data) as T;
-                    obs.next(data);
-                }
-            };
+            const createWebsocket = () => {
+                console.log('Started new websocket for ' + url);
+                const websocket = new WebSocket(url);
+                websocket.onmessage = event => {
+                    if (event.data === 'ping') {
+                        websocket.send('pong');
+                    } else {
+                        const data = JSON.parse(event.data) as T;
+                        obs.next(data);
+                    }
+                };
 
-            websocket.onclose = () => {
-              console.warn('Websocket for ' + url + ' closed.');
+                websocket.onclose = () => {
+                    console.warn('Websocket for ' + url + ' closed.');
+                    createWebsocket();
+                };
             };
+            createWebsocket();
 
             return () => { };
         }).pipe(share());
