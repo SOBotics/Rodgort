379525bdb00e1c2caec1b2bc1c42d6bb5fc0f283
Robert Rudman <rjrudman@gmail.com>
2019-02-18 17:39:59 +1100

    Don't continually make requests when user logs in/out

--- Rodgort/ClientApp/src/app/interceptors/HttpAuthenticationInterceptor.ts
+++ Rodgort/ClientApp/src/app/interceptors/HttpAuthenticationInterceptor.ts
@@ -3,13 +3,14 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/c
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../services/auth.service';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/first';
 
 @Injectable()
 export class HttpAuthenticationInterceptor implements HttpInterceptor {
     constructor(private authService: AuthService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return this.authService.GetAuthDetails().switchMap(d => {
+        return this.authService.GetAuthDetails().first().switchMap(d => {
             const modified =
                 d.IsAuthenticated
                     ? request.clone({ setHeaders: { 'Authorization': `Bearer ${d.RawToken}` } })
