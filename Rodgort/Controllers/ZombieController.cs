956118e171e04179b6fb278daae4f34c50a0c39d
Robert Rudman <rjrudman@gmail.com>
2019-02-22 18:22:26 +1100

    Gah, fix typo

--- Rodgort/Controllers/ZombieController.cs
+++ Rodgort/Controllers/ZombieController.cs
@@ -39,8 +39,7 @@ select
     tags t
     inner join tag_statistics a on t.name = a.tag_name
 	inner join meta_question_tags mqt on mqt.tag_name = t.name and mqt.tracking_status_id = 2
-    where 
-    and @tag is null and (@allZombies or t.number_of_questions > 0)
+    where @tag is null and (@allZombies or t.number_of_questions > 0)
     or t.name = @tag
 ) innerQuery
 where innerQuery.revived
