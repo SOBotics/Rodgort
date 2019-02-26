a579f1819105640ade0bde70b86e8bf711537e68
Robert Rudman <rjrudman@gmail.com>
2019-02-26 16:44:42 +1100

    Fix migration error

--- Rodgort/Migrations/20190226052847_RemoveDefaultValueForIsSynonym.cs
+++ Rodgort/Migrations/20190226052847_RemoveDefaultValueForIsSynonym.cs
@@ -6,22 +6,76 @@ namespace Rodgort.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql(@"
+DROP MATERIALIZED VIEW zombie_tags;
+");
+
             migrationBuilder.AlterColumn<bool>(
                 name: "is_synonym",
                 table: "tag_statistics",
                 nullable: false,
                 oldClrType: typeof(bool),
                 oldDefaultValue: false);
+
+            migrationBuilder.Sql(@"
+CREATE MATERIALIZED VIEW zombie_tags as (
+	select 
+		tag_name as tag_name,
+		date_time time_revived
+	 from (
+		select 
+			ts.tag_name,
+		    ts.date_time,
+			case 
+				when lag(question_count) over (partition by ts.tag_name order by ts.date_time) = 0 
+	            and not lag(is_synonym) over (partition by ts.tag_name order by ts.date_time)
+	            and question_count > 0 then true
+				else false
+			end 
+			as revived
+		from tag_statistics ts
+		inner join meta_question_tags mqt on mqt.tag_name = ts.tag_name and mqt.tracking_status_id = 2
+	) innerQuery
+	where innerQuery.revived
+);
+");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql(@"
+DROP MATERIALIZED VIEW zombie_tags;
+");
+
             migrationBuilder.AlterColumn<bool>(
                 name: "is_synonym",
                 table: "tag_statistics",
                 nullable: false,
                 defaultValue: false,
                 oldClrType: typeof(bool));
+
+            migrationBuilder.Sql(@"
+CREATE MATERIALIZED VIEW zombie_tags as (
+	select 
+		tag_name as tag_name,
+		date_time time_revived
+	 from (
+		select 
+			ts.tag_name,
+		    ts.date_time,
+			case 
+				when lag(question_count) over (partition by ts.tag_name order by ts.date_time) = 0 
+	            and not lag(is_synonym) over (partition by ts.tag_name order by ts.date_time)
+	            and question_count > 0 then true
+				else false
+			end 
+			as revived
+		from tag_statistics ts
+		inner join meta_question_tags mqt on mqt.tag_name = ts.tag_name and mqt.tracking_status_id = 2
+	) innerQuery
+	where innerQuery.revived
+);
+");
         }
     }
 }
